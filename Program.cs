f3f23b6f3b181bd12a55fc128a99b7d6d35c6cb2
agencyadmin <osmprobox@gmail.com>
2022-01-21 16:53:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Modul_8
+{
+    public class FilesSystemMy
+    {
+        public string DiskName { get; set; } = "C";
+        public string DiskValue { get; set; } = null;
+        public string DiskFreeSpace { get; set; }
+
+        public FilesSystemMy()
+        {
+            DiskName = "\\C:";
+            DiskValue = "512 G";
+            DiskFreeSpace = "512 Gb";
+        }
+    }
+}
