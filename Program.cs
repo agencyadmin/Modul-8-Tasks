08784eb148c5bdd5ff02b58348b305230d82c143
agencyadmin <osmprobox@gmail.com>
2022-01-21 17:12:43 +0300

    8,1,4,-8.1.5.

--- Program.cs
+++ Program.cs
@@ -3,14 +3,40 @@
     public class FilesSystemMy
     {
         public string DiskName { get; set; } = "C";
-        public string DiskValue { get; set; } = null;
-        public string DiskFreeSpace { get; set; }
+        public long DiskValue { get; set; } = 512;
+        public long DiskFreeSpace { get; set; }
 
         public FilesSystemMy()
         {
             DiskName = "\\C:";
-            DiskValue = "512 G";
-            DiskFreeSpace = "512 Gb";
+            DiskValue = 512;
+
+            DiskFreeSpace = 512;
+        }
+        public FilesSystemMy(string name, long totalSpace, long freeSpace)
+        {
+            DiskName = name;
+            DiskValue = totalSpace;
+            DiskFreeSpace = freeSpace;
+        }
+
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+        public string MakeNewFolder(string newFolderName)
+        {
+            Files.Add(newFolderName);
+            return "Новая папка создана";
+        }
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();  
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
         }
     }
+
+
 }
